ad3a1209c9dfce4b28f36ee74cd4bb747de0dc0a
FelipeeSantos <luizfelipedossantoss@gmail.com>
2021-07-12 19:46:59 -0300

    create_category

--- src/model/Category.ts
+++ src/model/Category.ts
@@ -1,3 +1,16 @@
-class Category {}
+import { v4 as uuidV4 } from "uuid";
+
+class Category {
+  id?: string;
+  name: string;
+  description: string;
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
 
 export { Category };
